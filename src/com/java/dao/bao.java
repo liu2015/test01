082e762db73855315db0cc7b118ad56d00670ed0
liu2015 <liufuling14@163.com>
2017-05-28 22:45:45 +0800

    初步完成dao编码

--- src/com/java/dao/bao.java
+++ src/com/java/dao/bao.java
@@ -1,43 +1,97 @@
 package com.java.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.mysql.jdbc.PreparedStatement;
+
+
+import java.sql.PreparedStatement;
 
 public class bao {
 
-	private String className="com.mysql.jdbc.Driver";
-	private String url="jdbc:mysql://localhost:3306/db_memomodule";
-	private String user="root";
-	private String password="liu1988226";
-	
-	protected Connection conn=null;
-	protected PreparedStatement ps=null;
-	protected ResultSet rs=null;
-	
-	public Connection getconnection(){
-	
-		try{
-			 Class.forName(className);
-			 
-			 
-		}catch(ClassNotFoundException e){
-			
+	private String className = "com.mysql.jdbc.Driver";
+	private String url = "jdbc:mysql://localhost:3306/db_memomodule";
+	private String user = "root";
+	private String password = "liu1988226";
+	protected Connection conn = null;
+	protected PreparedStatement ps = null;
+	protected ResultSet rs = null;
+
+	public Connection getconnection() {
+
+		try {
+			Class.forName(className);
+			conn= DriverManager.getConnection(url,user, password);
+
+		} catch (ClassNotFoundException e) {
+
 			e.printStackTrace();
-			
-			
-		}
-		catch(Exception e)
-		{
+
+		} catch (Exception e) {
 			e.printStackTrace();
-			
+
 		}
-		
-		
+
 		return conn;
+
+	}
+	
+	
+	
+	//插入,修改,删除等操作的命令,没有返回结果集合的都可以操作.
+	public void update (String sql ,Object[] wanneng ){
+		conn=this.getconnection();
+	try {
+	ps=conn.prepareStatement(sql);
+	
+	if(wanneng !=null)
+	{
+		for (int i=0;i<wanneng.length;i++){
+			ps.setObject(i+1, wanneng[i]);
+			
+			
+			
+			
+		}
 		
+	}
+	//执行sql语句
+		ps.executeQuery();
+	} catch (Exception e) {
+		// TODO: handle exception
+	}finally {
+		
+		this.clone(conn,ps,rs);
+	}
+		
+	}
+
+
+
+	private void clone(Connection conn2, PreparedStatement ps2, ResultSet rs2) {
+		// TODO Auto-generated method stub
+		try {
+			if(rs!=null){
+				rs.close();
+				rs=null;
+			}if(ps!=null)
+			{
+				ps.close();
+				ps=null;
+				
+				
+			}
+			if(conn!=null){
+				conn.close();
+				conn=null;
+				
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 		
 		
 		
@@ -45,4 +99,5 @@ public class bao {
 	
 	
 	
+
 }
