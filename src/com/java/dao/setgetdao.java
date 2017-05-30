ddfddf8a6753ab841ce61a65849df696e44c4450
liu2015 <liufuling14@163.com>
2017-05-30 21:09:49 +0800

    添加setget,放置,获得字段

--- src/com/java/dao/setgetdao.java
+++ src/com/java/dao/setgetdao.java
@@ -1,7 +1,46 @@
 package com.java.dao;
 
+import java.sql.Date;
+
 public class setgetdao {
 
+	private int id;
+	private String username;
+	private String content;
+	private String memotype;
+	private Date memotime;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getMemotype() {
+		return memotype;
+	}
+	public void setMemotype(String memotype) {
+		this.memotype = memotype;
+	}
+	public Date getMemotime() {
+		return memotime;
+	}
+	public void setMemotime(Date memotime) {
+		this.memotime = memotime;
+	}
+
+	
 	
 	
 }
